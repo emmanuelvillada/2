05ce19d8339206fa2580b785cb96c8a539502387
salak401 <salak401@SALAK401-4>
2023-02-20 19:25:01 -0500

    Add project files.

---
+++ 2/ComissionEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class ComissionEmployee
+    {
+        public float ComissionPorcentage { get; set; }
+        public decimal Sales { get; set; }
+
+        public decimal GetValueToPay()
+        {
+            return Sales * (decimal)ComissionPorcentage / 100;
+
+        }
+
+
+        public override string ToString()
+        {
+            return $" this is the comision :{ComissionPorcentage}  this are the sales{Sales :c2}  this is the comission : {GetValueToPay()}   ";
+        }
+  
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _2;
+
+Console.WriteLine("enter your sales");
+decimal Sales = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter your comission porcentage");
+float ComissionPorcentage = Convert.ToSingle(Console.ReadLine());
+
+var objEm = new ComissionEmployee() {
+    Sales = Sales,
+    ComissionPorcentage= ComissionPorcentage,   
+
+        }; 
+Console.WriteLine(objEm.ToString());
